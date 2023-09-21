aaa833367e33947020a8fbb79b81c48b1e2aba68
Brett Tanonaka <tano.brett@gmail.com>
2023-09-20 17:01:30 -0700

    Leaderboard sorts data and renders each user with their total uploads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './Components/LandingPage';
+import Leaderboard from './Components/Leaderboard';
 
 function App() {
   return (
     <div className="App">
       <LandingPage/>
+      <Leaderboard/>
     </div>
   );
 }
--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { userPictureData as pictureData, userData } from '../sampleLeaderboardData.js'
 
 export default function Leaderboard() {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+
+  const renderTable = (user, index) => (
+    <tr>
+      <td>{index + 1}</td>
+      <td>{user.username}</td>
+      <td>{user.totalUploads}</td>
+    </tr>
+  )
+
+  useEffect(() => {
+    pictureData.sort((a, b) => (b.totalUploads - a.totalUploads))
+    setLeaderboardData(pictureData);
+  }, [leaderboardData])
+
   return (
-    <div></div>
+    <div className="leaderboard-container">
+      <table>
+        <tr>
+          <th scope="col">Rank</th>
+          <th scope="col">Name</th>
+          <th scope="col">Total</th>
+        </tr>
+        {leaderboardData.map((user, index) => (renderTable(user, index)))}
+      </table>
+    </div>
   )
 }
