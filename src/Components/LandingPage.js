3decf2c4cdbeb4c59bcb8c62e21dfeefe259dbb1
Eric Skelly <97416175+skellyska@users.noreply.github.com>
2023-10-21 18:46:35 -0700

    updated LandingPage.js, added LandingPage.css, and styled LandingPage.js

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../LandingPage.css';
+import Image from '../landingPage.png';
 
 function LandingPage () {
     return (
-        <div>
-            <h1>Welcome to Litter.ai</h1>
-            <p>With just a click, sort your garbage and save the world</p>
-            <div style={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)', width: '200px' }}>
-                <button>Learn More</button>
+        <div className='landing-page-container'>
+            <img src={Image} />
+            <div className="landing-page-text">
+                <h1 className="pageTitle">Welcome to Litter.ai</h1>
+                <p className="pageDescription">With just a click, sort your garbage and save the world</p>
+            </div>
+            <div className="landing-page-buttons">
+                <button className="button">Learn More</button>
+                <br></br><br></br>
                 <Link to="/capture">
-                    <button>Capture Image</button>
+                    <button className="button">Capture Image</button>
                 </Link>
             </div>
         </div>
