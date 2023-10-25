84a4eb1872607facc5dd85104e380810f322e5fd
Brett Tanonaka <tano.brett@gmail.com>
2023-10-25 16:29:33 -0700

    Changed body to display-flex to center components

--- src/Components/CategoryDescription.js
+++ src/Components/CategoryDescription.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import RecyclingIcon from '@mui/icons-material/Recycling';
+import '../CategoryDescription.css'
 
 export default function CategoryDescription ({ type }) {
 
   return (
-    <div>
+    <div className="category-container">
       <h4>Hooray!!!!</h4>
       <h4>You made it</h4>
       {/* <img src={category.icon} alt={category.alt} /> */}
-      <RecyclingIcon />
+      <RecyclingIcon className="category-icon" />
       <div>{type.description}</div>
-      <button>Learn More</button>
-      <Link to="/capture"><button>Capture another photo</button></Link>
-      <Link to="/"><button>Home</button></Link>
+      <div>
+        <button>Learn More</button>
+        <Link to="/capture"><button>Capture another photo</button></Link>
+        <Link to="/"><button className="button-home">Home</button></Link>
+      </div>
     </div>
   );
 };
