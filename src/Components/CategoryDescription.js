142472e330f37f859d01e107037769ab2de2ba36
Brett Tanonaka <tano.brett@gmail.com>
2023-10-25 15:57:25 -0700

    Connected routes for capture and home in CategoryDescription

--- src/Components/CategoryDescription.js
+++ src/Components/CategoryDescription.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import RecyclingIcon from '@mui/icons-material/Recycling';
 
 export default function CategoryDescription ({ type }) {
-  const [category] = useState(type);
 
   return (
     <div>
       <h4>Hooray!!!!</h4>
       <h4>You made it</h4>
-      <img src={category.icon} alt="recycle icon" />
-      <div>{category.description}</div>
+      {/* <img src={category.icon} alt={category.alt} /> */}
+      <RecyclingIcon />
+      <div>{type.description}</div>
+      <button>Learn More</button>
+      <Link to="/capture"><button>Capture another photo</button></Link>
+      <Link to="/"><button>Home</button></Link>
     </div>
   );
 };
--- src/DummyData/sampleCategoryData.js
+++ src/DummyData/sampleCategoryData.js
@@ -1,16 +1,17 @@
-import RecyclingIcon from '@mui/icons-material/Recycling';
-
 export const categoryData = {
   recycle: {
-    icon: RecyclingIcon,
+    icon: '',
+    alt: 'recycle icon',
     description: 'This is recycle. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam volutpat.'
   },
   compost: {
     icon: '',
+    alt: 'compost icon',
     description: 'This is compost. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam volutpat.'
   },
   trash: {
     icon: '',
+    alt: 'trash icon',
     description: 'This is trash. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam volutpat.'
   },
 }
