9e1e3783d448d052be8f7fc942bb37efa9e8f06e
Brett Tanonaka <tano.brett@gmail.com>
2023-10-25 17:23:23 -0700

    Added button hover effects; refactored buttons to have a max width

--- src/Components/CategoryDescription.js
+++ src/Components/CategoryDescription.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import RecyclingIcon from '@mui/icons-material/Recycling';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EggIcon from '@mui/icons-material/Egg';
 import '../CategoryDescription.css'
 
 export default function CategoryDescription ({ type }) {
+  const [category] = useState(type.name);
 
   return (
     <div className="category-container">
       <h4>Thank you!</h4>
-      {/* <img src={category.icon} alt={category.alt} /> */}
-      <RecyclingIcon className="category-icon" />
+      {category === 'Recycle' && <RecyclingIcon className="category-icon" />}
+      {category === 'Trash' && <DeleteIcon className="category-icon" />}
+      {category === 'Compost' && <EggIcon className="category-icon" />}
       <h1>{type.name}</h1>
       <p>{type.description}</p>
       <div className="category-buttons">
