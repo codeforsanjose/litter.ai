3e6a0a239ab18615b31de9e8921541cdcbfaf043
Brett Tanonaka <tano.brett@gmail.com>
2023-11-14 11:45:25 -0800

    Leaderboard header and rows change font-size based on screen size

--- src/Components/Leaderboard/Dropdown.js
+++ src/Components/Leaderboard/Dropdown.js
@@ -14,6 +14,31 @@ const options = [
   { value: 'unknown', label: 'Unknown' },
 ]
 
+const reactSelectStyles = {
+  // Text label
+  singleValue: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: '#076B49',
+    color: 'white',
+  }),
+  // Text border and background
+  control: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: '#076B49',
+    border: 'none',
+    width: '100%',
+  }),
+  // Down arrow
+  dropdownIndicator: (baseStyles) => ({
+    ...baseStyles,
+    color: '#FFF',
+    "@media only screen and (max-width: 767px)": {
+      ...baseStyles["@media only screen and (max-width: 767px)"],
+      width: '1.5rem'
+    }
+  }),
+}
+
 export default function Dropdown({ setCategory }) {
   const handleChange = (option) => {
     setCategory(option.value);
@@ -25,26 +50,7 @@ export default function Dropdown({ setCategory }) {
       defaultValue={options[0]}
       onChange={handleChange}
       isSearchable={false}
-      styles={{
-        // Text label
-        singleValue: (baseStyles) => ({
-          ...baseStyles,
-          backgroundColor: '#076B49',
-          color: 'white',
-        }),
-        // Text border and background
-        control: (baseStyles) => ({
-          ...baseStyles,
-          backgroundColor: '#076B49',
-          border: 'none',
-          width: '100%',
-        }),
-        // Down arrow
-        dropdownIndicator: (baseStyles) => ({
-          ...baseStyles,
-          color: '#FFF',
-        }),
-      }}
+      styles={reactSelectStyles}
     />
   )
 }
