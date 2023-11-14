4eae485cab3d0a0ce151675226ed1dd28eafe4de
Brett Tanonaka <tano.brett@gmail.com>
2023-11-14 12:45:18 -0800

    Leaderboard dropdown menu changes colors based on which option is selected

--- src/Components/Leaderboard/Dropdown.js
+++ src/Components/Leaderboard/Dropdown.js
@@ -15,18 +15,35 @@ const options = [
 ]
 
 const reactSelectStyles = {
-  // Text label
+  // Table header text label
   singleValue: (baseStyles) => ({
     ...baseStyles,
     backgroundColor: '#076B49',
-    color: 'white',
+    color: '#FFF',
   }),
-  // Text border and background
+  // Table header text border and background
   control: (baseStyles) => ({
     ...baseStyles,
     backgroundColor: '#076B49',
     border: 'none',
     width: '100%',
+  }),
+  // Dropdown menu
+  option: (baseStyles, state) => ({
+    ...baseStyles,
+    backgroundColor:
+    state.isSelected
+    ? '#076B49'
+    : state.isFocused
+    ? '#74CC67' : '#FFF',
+    color: state.isSelected ? '#FFF' : '#000',
+    '&:hover': {
+      color: '#FFF',
+    },
+    ':active': {
+      ...baseStyles[':active'],
+      backgroundColor: '#0c9265',
+    }
   }),
   // Down arrow
   dropdownIndicator: (baseStyles) => ({
@@ -35,7 +52,7 @@ const reactSelectStyles = {
     "@media only screen and (max-width: 767px)": {
       ...baseStyles["@media only screen and (max-width: 767px)"],
       width: '1.5rem'
-    }
+    },
   }),
 }
 
