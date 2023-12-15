df33b37ef4e5158639a3d5e3532d8f6a5cb183a6
Tim Engle <timothy.englew@gmail.com>
2023-12-14 20:31:57 -0800

    Added isAuthAccess to check access token. Implemented in photo route

--- backend/routes/photo.js
+++ backend/routes/photo.js
@@ -1,12 +1,16 @@
 import express from 'express';
 
 import controllers from '../controllers/index.js';
-import isAuth from '../middleware/isAuth.js';
+import isAuthAccess from '../middleware/is-auth-access-token.js';
 
 const photoRoutes = express.Router();
 
-photoRoutes.post('/', isAuth, controllers.photoInfo.postPhoto);
+photoRoutes.post('/', isAuthAccess, controllers.photoInfo.postPhoto);
 
-photoRoutes.get('/:username', isAuth, controllers.photoInfo.getUserPhotoCount);
+photoRoutes.get(
+    '/:username',
+    isAuthAccess,
+    controllers.photoInfo.getUserPhotoCount,
+);
 
 export default photoRoutes;
