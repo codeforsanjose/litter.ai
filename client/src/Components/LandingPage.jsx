f4937947d74bfb9e15277b205ac6e7ff8c40f2fc
Brett Tanonaka <tano.brett@gmail.com>
2023-12-18 12:27:49 -0800

    Landing page has a full colored background

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -5,32 +5,35 @@ import '../css/Landing.css';
 
 export default function LandingPage() {
   return (
-    <div className="landing-container main-container">
-      <PiFlowerTulipThin className="landing-logo" />
-      <div className="landing-bottom-section">
-        <div className="landing-text">
-          <h1>Welcome to LitterSort</h1>
-          <p>With just a photo, sort your garbage and save the world.</p>
-        </div>
-        <div className="landing-buttons lower-buttons">
-          <Link to="/capture">
-            <button type="button" className="landing-capture-button">
-              Capture Picture
-            </button>
-          </Link>
-          <Link to="/register">
-            <button type="button" className="landing-register-button">
-              Sign Up
-            </button>
-          </Link>
-          <div className="landing-login-text">
-            <p>Already have an account?</p>
-            <Link to="/login">
-              Log in
+    <>
+      <div className="landing-container main-container">
+        <PiFlowerTulipThin className="landing-logo" />
+        <div className="landing-bottom-section">
+          <div className="landing-text">
+            <h1>Welcome to LitterSort</h1>
+            <p>With just a photo, sort your garbage and save the world.</p>
+          </div>
+          <div className="landing-buttons lower-buttons">
+            <Link to="/capture">
+              <button type="button" className="landing-capture-button">
+                Capture Picture
+              </button>
             </Link>
+            <Link to="/register">
+              <button type="button" className="landing-register-button">
+                Sign Up
+              </button>
+            </Link>
+            <div className="landing-login-text">
+              <p>Already have an account?</p>
+              <Link to="/login">
+                Log in
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="landing-full-bg" />
+    </>
   );
 }
