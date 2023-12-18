1baeb9334b4037bfec0a1111fbcc9314f0bd1603
Brett Tanonaka <tano.brett@gmail.com>
2023-12-18 15:37:18 -0800

    Camera sends file data when uploaded

--- client/src/Components/CameraCapture.jsx
+++ client/src/Components/CameraCapture.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-function CameraCapture() {
+export default function CameraCapture() {
+  const [image, setImage] = useState(null);
+  const inputRef = useRef(null);
+  const handleImage = (e) => {
+    const imageData = [e.target.files[0]];
+    setImage({
+      imagePreview: URL.createObjectURL(new Blob(imageData, { type: 'application/zip' })),
+      imageFile: e.target.files[0],
+    });
+  };
+  const showImageData = () => {
+    console.log('Image Data: ', image);
+  };
+
   return (
     <div>
       <input
         type="file"
-        accept=".jpg, .jpeg, .img, .HEIC"
+        accept="image/*"
+        ref={inputRef}
         id="cameraFileInput"
         capture="environment"
+        onChange={handleImage}
       />
-      <img id="pictureFromCamera" alt="" />
+      <button type="button" onClick={() => inputRef.current.click()}>Confirm</button>
+      <button type="button" onClick={showImageData}>Show image data</button>
+      {image && <img id="pictureFromCamera" alt="" src={image.imagePreview} />}
       <script src="ImageCapture.js" />
     </div>
   );
 }
-
-export default CameraCapture;
