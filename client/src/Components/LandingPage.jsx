f71657dbd832a1ea29222a79b89b0f4a2bc2578f
Brett Tanonaka <tano.brett@gmail.com>
2023-12-18 10:38:11 -0800

    Created base structure of the landing page

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { TbWorldHeart } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Welcome to Litter.ai</h1>
-      <p>With just a click, sort your garbage and save the world</p>
-      <div style={{
-        position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)', width: '200px',
-      }}
-      >
-        <button type="button">Learn More</button>
+    <div className="landing-container">
+      <TbWorldHeart />
+      <div>
+        <h1>Welcome to Litter.ai</h1>
+        <p>With just a click, sort your garbage and save the world</p>
+      </div>
+      <div className="landing-buttons lower-buttons">
         <Link to="/capture">
-          <button type="button">Capture Image</button>
+          <button type="button">
+            Capture Picture
+          </button>
+        </Link>
+        <Link to="/register">
+          <button className="button-home" type="button">
+            Sign Up
+          </button>
+        </Link>
+        <p>Already have an account?</p>
+        <Link to="/login">
+          <a href="/login">
+            Log in
+          </a>
         </Link>
       </div>
     </div>
