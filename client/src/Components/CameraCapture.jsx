8dddd1712ded544c65308590234d168e6800f877
Brett Tanonaka <tano.brett@gmail.com>
2023-12-18 17:17:29 -0800

    Changed upload file button to camera icon

--- client/src/Components/CameraCapture.jsx
+++ client/src/Components/CameraCapture.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useRef } from 'react';
+import { IoCameraOutline } from 'react-icons/io5';
 
 export default function CameraCapture() {
   const [image, setImage] = useState(null);
-  const inputRef = useRef(null);
+  const captureRef = useRef(null);
+
+  const openCamera = () => {
+    captureRef.current.click();
+  };
+
   const handleImage = (e) => {
     const imageData = [e.target.files[0]];
     setImage({
@@ -10,23 +16,26 @@ export default function CameraCapture() {
       imageFile: e.target.files[0],
     });
   };
-  const showImageData = () => {
+  const handlePhotoSubmit = () => {
     console.log('Image Data: ', image);
   };
 
   return (
-    <div>
+    <div className="capture-container main-container">
+      <IoCameraOutline onClick={openCamera} />
       <input
         type="file"
         accept="image/*"
-        ref={inputRef}
         id="cameraFileInput"
+        ref={captureRef}
         capture="environment"
         onChange={handleImage}
+        style={{ display: 'none' }}
       />
-      <button type="button" onClick={() => inputRef.current.click()}>Confirm</button>
-      <button type="button" onClick={showImageData}>Show image data</button>
-      {image && <img id="pictureFromCamera" alt="" src={image.imagePreview} />}
+      <button type="button" onClick={handlePhotoSubmit}>Confirm</button>
+      <div className="capture-image-preview">
+        {image && <img id="pictureFromCamera" alt="" src={image.imagePreview} />}
+      </div>
       <script src="ImageCapture.js" />
     </div>
   );
