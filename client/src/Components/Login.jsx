2c646a3fa1e2034713b2b352cde791907cd9357e
Jessica <jessicatong43@gmail.com>
2023-12-22 18:45:52 -0800

    🔓Added Back button and CSS to login page.

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import { Link } from 'react-router-dom';
+import { FaAngleLeft, FaRegEnvelope } from 'react-icons/fa6';
+import { FiLock } from 'react-icons/fi';
 import { fetchLogin, fetchLogOut } from '../utils/fetchUserData';
 import '../css/Login.css';
-import logo from '../images/Logo_large_white.png';
 
 export default function Login({ user, setUser }) {
   const [loginData, setLoginData] = useState({
@@ -39,71 +40,86 @@ export default function Login({ user, setUser }) {
 
   return (
     <div className="login-container main-container">
-      <div className="login-logo">
-        <img className="login-logo-image" src={logo} alt="Logo" />
+      <div className="login-header">
+        <Link to="/" className="back-button">
+          <button className="back-button" type="button" aria-label="Back">
+            <FaAngleLeft />
+          </button>
+        </Link>
+        <div className="login-header-text">
+          <h3>Sign In</h3>
+          <p>Enter your credentials to continue</p>
+        </div>
       </div>
       <form className="login-form">
-        {invalidLogin && <p className="invalid-login">Incorrect username and/or password</p> }
+        {invalidLogin
+          ? (
+            <div className="invalid-login">
+              <small>Incorrect username and/or password</small>
+            </div>
+          )
+          : <br className="invalid-login" />}
         <div className="login-email">
-          <label htmlFor="email">
-            Email:&nbsp;
-            <input
-              type="text"
-              id="email"
-              value={loginData.email}
-              onChange={(e) => handleChange(e)}
-            />
-          </label>
+          <FaRegEnvelope className="login-icon" />
+          <input
+            type="text"
+            id="email"
+            aria-label="Email"
+            placeholder="Email"
+            value={loginData.email}
+            onChange={(e) => handleChange(e)}
+          />
         </div>
+        <br className="login-form-break" />
         <div className="login-password">
-          <label htmlFor="password">
-            Password:&nbsp;
-            <input
-              type="password"
-              id="password"
-              value={loginData.password}
-              onChange={(e) => handleChange(e)}
-            />
-          </label>
+          <FiLock className="login-icon" />
+          <input
+            type="password"
+            id="password"
+            aria-label="Password"
+            placeholder="Password"
+            value={loginData.password}
+            onChange={(e) => handleChange(e)}
+          />
         </div>
-        <div className="lower-buttons">
-          <button
-            className="login-button"
-            type="button"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-          <button
-            className="login-button"
-            type="button"
-            onClick={handleCheck}
-          >
-            Test Login Data
-          </button>
-          <button
-            className="login-button"
-            type="button"
-            onClick={async () => {
-              await fetchLogOut();
-              await setUser(null);
-            }}
-          >
-            Log out
-          </button>
-          <Link to="/leaderboard">
-            <button className="login-button" type="button">Leaderboard</button>
-          </Link>
-          <Link to="/profile">
-            <button className="login-button" type="button">Profile</button>
-          </Link>
-          <Link to="/">
-            <button className="button-home" type="button">
-              Home
-            </button>
-          </Link>
+        <div className="forgot-password">
+          <small>Forgot Password?</small>
         </div>
       </form>
+      <div className="lower-buttons login-buttons">
+        <button
+          type="button"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          onClick={handleCheck}
+        >
+          Test Login Data
+        </button>
+        <button
+          type="button"
+          onClick={async () => {
+            await fetchLogOut();
+            await setUser(null);
+          }}
+        >
+          Log out
+        </button>
+        <Link to="/leaderboard">
+          <button type="button">Leaderboard</button>
+        </Link>
+        <Link to="/profile">
+          <button type="button">Profile</button>
+        </Link>
+        <Link to="/">
+          <button className="button-home" type="button">
+            Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
