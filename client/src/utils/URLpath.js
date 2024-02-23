fd1ebe34beba10579127a2105d498873df97b47a
Brett Tanonaka <tano.brett@gmail.com>
2024-02-22 17:12:06 -0800

    Removed direct links to api/ai

--- client/src/utils/URLpath.js
+++ client/src/utils/URLpath.js
@@ -1,7 +1,7 @@
-export const aiEndpoint = process.env.REACT_APP_AI_KEY || 'https://aii.littersort.ai';
+export const aiEndpoint = process.env.REACT_APP_AI_KEY;
 
 export default function URLpath(endpoint, extension) {
-  const deployed = process.env.REACT_APP_API_KEY || 'https://api.littersort.ai';
+  const deployed = process.env.REACT_APP_API_KEY;
   // const local = 'http://localhost:3001';
   const link = deployed;
   return extension
